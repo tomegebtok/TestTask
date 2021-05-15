7e94903cf3dc143f668393502ce95761e25956b2
tomegebtok <alexander.bordukov@yandex.ru>
2021-05-15 19:37:30 +0300

    final

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Solution {
+public class Main {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
 
